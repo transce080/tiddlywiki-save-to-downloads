8d927b48310f6ba21b67ee6a482f0634273ec4c0
Jason Lonsberry <33551248+transce080@users.noreply.github.com>
2025-10-19 19:51:32 -0400

    Linting and tidying

--- extension/options.js
+++ extension/options.js
@@ -1,85 +1,90 @@
+/* globals browser, chrome */
+'use strict'
+
+const SAVE_OPTIONS_TIMEOUT = 750
+const CHECK_TIMEOUT = 2750
+
 // Saves options to chrome.storage.sync.
 function save_options() {
   if (!check(document.getElementById('homedir'))) return
   if (!check(document.getElementById('backupdir'))) return
+
   chrome.storage.local.set({
-    homedir: document.getElementById('homedir').value,
     backupdir: document.getElementById('backupdir').value,
-    backuptw5: document.getElementById("backuptw5").checked,
-    backuptwc: document.getElementById("backuptwc").checked,
-    nag: document.getElementById("nag").checked
-  }, function () {
+    backuptw5: document.getElementById('backuptw5').checked,
+    backuptwc: document.getElementById('backuptwc').checked,
+    homedir: document.getElementById('homedir').value,
+    nag: document.getElementById('nag').checked
+  }, () => {
     // Update status to let user know options were saved.
-    var status = document.getElementById('status')
+    const status = document.getElementById('status')
     status.textContent = 'Options saved.'
-    setTimeout(function () {
-      status.textContent = ''
-    }, 750)
+    setTimeout(() => { status.textContent = '' }, SAVE_OPTIONS_TIMEOUT)
   })
 }
 
-
 //from https://github.com/parshap/node-sanitize-filename/
-var illegalRe = /[\/\?<>\\:\*\|":]/
-var ctlRe = /[\x00-\x1f\x80-\x9f]/
-var rvRe = /^\.+$/
-var winRsRe = /^(con|prn|aux|nul|com[0-9]|lpt[0-9])(\..*)?$/i
-var winTrRe = /[\. ]+$/
+const illegalRegex = /[/?<>\\:*|":]/u
+const ctlRegex = /[\x00-\x1f\x80-\x9f]/u
+const rvRegex = /^\.+$/u
+const winRsRegex = /^(con|prn|aux|nul|com[0-9]|lpt[0-9])(\..*)?$/iu
+const winTrRegex = /[. ]+$/u
 
-var unixRe = /[\/]/
-var unixdotsRe = /^\.|\.\.$/
+const unixRe = /[/]/u
+const unixDotsRe = /^\.|\.\.$/u
 
-var test
+let test
 
-chrome.runtime.getPlatformInfo(function (info) {
-  if (info.os === "win") {
-    test = function (value) {
-      return (value.match(illegalRe) || value.match(ctlRe)
-        || value.match(rvRe) || value.match(winRsRe) || value.match(winTrRe))
-    }
+chrome.runtime.getPlatformInfo((info) => {
+  if (info.os === 'win') {
+    test = (value) => (value.match(illegalRegex) || value.match(ctlRegex)
+      || value.match(rvRegex) || value.match(winRsRegex) || value.match(winTrRegex))
   } else {
-    test = function (value) { return (value.match(unixRe) || value.match(unixdotsRe)) }
+    test = (value) => (value.match(unixRe) || value.match(unixDotsRe))
   }
 })
-function check(element) {
 
-  if (!test(element.value)) {
-    return true
-  } else {
-    var error = document.getElementById('error')
+function check(element) {
+  if (test(element.value)) {
+    const error = document.getElementById('error')
     error.textContent = 'name invalid.'
     element.focus()
-    setTimeout(function () {
+
+    setTimeout(() => {
       error.textContent = ''
-    }, 2750)
+    }, CHECK_TIMEOUT)
+
     return false
   }
+
+  return true
 }
+
 // Restores select box and text fields
 function restore_options() {
   try {
-    browser.runtime.getBrowserInfo(function (info) {
-      if (info.vendor != "Mozilla") document.getElementById('showand').hidden = false
+    browser.runtime.getBrowserInfo((info) => {
+      if (info.vendor != 'Mozilla')
+        document.getElementById('showand').hidden = false
     })
-  } catch (e) { document.getElementById('showand').hidden = false }
-
-
+  } catch {
+    document.getElementById('showand').hidden = false
+  }
 
   chrome.storage.local.get({
-    homedir: "tiddlywikilocations",
-    backupdir: "backupdir",
+    backupdir: 'backupdir',
     backuptw5: true,
     backuptwc: false,
+    homedir: 'tiddlywikiLocations',
     nag: true
-  }, function (items) {
+  }, (items) => {
     document.getElementById('homedir').value = items.homedir
     document.getElementById('backupdir').value = items.backupdir
-    document.getElementById("backuptw5").checked = items.backuptw5
-    document.getElementById("backuptwc").checked = items.backuptwc
-    document.getElementById("nag").checked = items.nag
+    document.getElementById('backuptw5').checked = items.backuptw5
+    document.getElementById('backuptwc').checked = items.backuptwc
+    document.getElementById('nag').checked = items.nag
   })
 }
 
 document.addEventListener('DOMContentLoaded', restore_options)
-document.getElementById('save').addEventListener('click',
-  save_options)
+document.getElementById('save').addEventListener('click', save_options)
